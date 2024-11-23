c085a8810ac8355c691ed9e4c2eee7c1cd400862
egido-quan <isidro.zapater@gmail.com>
2024-11-23 22:00:13 +0100

    Lógica de la función buscar pasada al controlador. Permite búsquedas no estrictas no exactas

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -61,6 +61,46 @@ class ApplicationController extends Controller
 
     public function confBuscarAction() {
 
+        $nuevaToDoList = getData();
+
+        $busqueda = [
+            "id"=>$_POST["id"],
+            "tarea"=>$_POST["tarea"],
+            "responsable"=>$_POST["responsable"],
+            "estado"=>$_POST["estado"],
+            "inicio"=>$_POST["inicio"],
+            "fin"=>$_POST["fin"]
+        ];
+
+        $resultado = [];
+
+        foreach ($nuevaToDoList as $dato) {
+            $j = 0;
+            if ($busqueda["id"] == "" || $busqueda["id"] == $dato["id"]) {
+                $j ++;
+            }
+            if ($busqueda["tarea"] == "" || str_contains(strtolower($dato["tarea"]), strtolower($busqueda["tarea"]))) {
+                $j ++;
+            }
+            if ($busqueda["responsable"] == "" ||
+                str_contains(self::arreglar($dato["responsable"]), self::arreglar($busqueda["responsable"]))) {
+                $j ++;
+            }
+            if ($busqueda["estado"] == "" || $busqueda["estado"] == $dato["estado"]) {
+                $j ++;
+            }
+            if ($busqueda["inicio"] == "" || $busqueda["inicio"] == $dato["inicio"]) {
+                $j ++;
+            }
+            if ($busqueda["fin"] == "" || $busqueda["fin"] == $dato["fin"]) {
+                $j ++;
+            }
+         
+            if ($j == 6) {
+                $resultado [] = $dato;
+            }        
+        }
+        $this->view->resultado = $resultado;
     }
     public function borrarListaAction() {
 
@@ -69,7 +109,7 @@ class ApplicationController extends Controller
     public function confBorrarListaAction() {
         $nuevaToDoList = [];
         $data_json =  json_encode($nuevaToDoList, JSON_PRETTY_PRINT);
-        $archivo = __DIR__ . "/../models/data/data.json";;
+        $archivo = __DIR__ . "/../models/data/data.json";
         file_put_contents($archivo, $data_json);  
     }
 
@@ -82,6 +122,12 @@ class ApplicationController extends Controller
         $data_json =  json_encode($sampleData, JSON_PRETTY_PRINT);
         $archivo = __DIR__ . "/../models/data/data.json";
         file_put_contents($archivo, $data_json); 
+    }
+
+    public function arreglar ($texto): string {
+        $texto = iconv('UTF-8', 'ASCII//TRANSLIT', $texto);
+        $texto = preg_replace('/[^a-zA-Z0-9\s]/', '', $texto);
+        return strtolower($texto);
     }
 
 }
